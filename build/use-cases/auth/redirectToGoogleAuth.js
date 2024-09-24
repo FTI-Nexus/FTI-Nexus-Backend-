9e61ed6e30841d792dabc5b540042f11a5d000a7
herberth arthur <herbertharthur80@gmail.com>
2024-09-24 01:13:03 +0000

    Code Refactor

--- build/use-cases/auth/redirectToGoogleAuth.js
+++ build/use-cases/auth/redirectToGoogleAuth.js
@@ -11,6 +11,6 @@ const redirectToGoogleAuth = (res, authType = "signup") => {
     const scope = authType === "signup"
         ? ["profile", "email", "https://www.googleapis.com/auth/user.birthday.read", "https://www.googleapis.com/auth/user.gender.read", "https://www.googleapis.com/auth/user.phonenumbers.read"]
         : ["email"];
-    res.redirect(authControllers_1.oauth2Client.generateAuthUrl({ scope, include_granted_scopes: true, state: process.env.GoogleOAuthStateValue }));
+    res.redirect(authControllers_1.oauth2Client.generateAuthUrl({ scope, include_granted_scopes: true, state: authType === "signup" ? process.env.GoogleOAuthStateValueForSignup : process.env.GoogleOAuthStateValueForLogin }));
 };
 exports.redirectToGoogleAuth = redirectToGoogleAuth;
--- src/use-cases/auth/redirectToGoogleAuth.ts
+++ src/use-cases/auth/redirectToGoogleAuth.ts
@@ -9,5 +9,7 @@ export const redirectToGoogleAuth =  (res:Response,authType:"login"|"signup"="si
       ? ["profile", "email", "https://www.googleapis.com/auth/user.birthday.read", "https://www.googleapis.com/auth/user.gender.read", "https://www.googleapis.com/auth/user.phonenumbers.read"]
       : ["email"];
 
-  res.redirect(oauth2Client.generateAuthUrl({ scope, include_granted_scopes: true, state: process.env.GoogleOAuthStateValue }));
+  res.redirect(
+    oauth2Client.generateAuthUrl({ scope, include_granted_scopes: true, state: authType === "signup" ? process.env.GoogleOAuthStateValueForSignup : process.env.GoogleOAuthStateValueForLogin })
+  );
 };
